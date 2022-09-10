5fe685616a151228f39e5bfbbdb3c8caa4c5bb89
felipeperaltacheung <f33___@hotmail.com>
2022-09-10 20:43:06 -0300

    Classe ExceptionResponse; UnsupportedMathOperationException para retornar BadRequest e a classe Handler para tratar as exceptios genericas

--- 0aAWS/src/main/java/br/com/erudio/_0aaws/MathController.java
+++ 0aAWS/src/main/java/br/com/erudio/_0aaws/MathController.java
@@ -1,5 +1,6 @@
 package br.com.erudio._0aaws;
 
+import br.com.erudio._0aaws.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,7 +19,7 @@ public class MathController {
             ) throws Exception {
 
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Plz set a numeric value");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
