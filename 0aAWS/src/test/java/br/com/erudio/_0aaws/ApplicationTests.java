e4e78e8dfd4b026f938c90aca8384af6815c9b27
felipeperaltacheung <f33___@hotmail.com>
2022-09-10 15:44:54 -0300

    Firsts Steps in Java with Spring Boot 3

---
+++ 0aAWS/src/test/java/br/com/erudio/_0aaws/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio._0aaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
