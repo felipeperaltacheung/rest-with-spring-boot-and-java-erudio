a463c0f57553f869c35acbbe684465d96d84b5a5
felipeperaltacheung <f33___@hotmail.com>
2022-09-17 10:39:57 -0300

    Configuracao com banco de dados postgres

--- 0aAWS/src/main/java/br/com/erudio/_0aaws/model/Person.java
+++ 0aAWS/src/main/java/br/com/erudio/_0aaws/model/Person.java
@@ -1,13 +1,23 @@
 package br.com.erudio._0aaws.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+    @Column(nullable = false, length = 100)
     private String address;
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {
