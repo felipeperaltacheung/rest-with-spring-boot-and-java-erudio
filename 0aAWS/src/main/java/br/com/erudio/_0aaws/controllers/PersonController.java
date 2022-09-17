a3615c022b5c72275e75c1037f202b14a3743351
felipeperaltacheung <f33___@hotmail.com>
2022-09-17 16:07:38 -0300

    refatorando os metodos de request

--- 0aAWS/src/main/java/br/com/erudio/_0aaws/controllers/PersonController.java
+++ 0aAWS/src/main/java/br/com/erudio/_0aaws/controllers/PersonController.java
@@ -12,42 +12,35 @@ import java.util.List;
 @RequestMapping("/person")
 public class PersonController {
 
-    //private PersonService service = new PersonService
     private final PersonService personService;
     @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
-    @RequestMapping(method=RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return personService.findAll();
     }
 
-    @RequestMapping(value = "/{id}",
-            method=RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id){
         return personService.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) {
         return personService.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person) {
         return personService.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable(value = "id") Long id) {
        personService.delete(id);
     }
